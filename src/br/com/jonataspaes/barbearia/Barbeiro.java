5963696eaa8146293a0c29913c69a25e2a42c3af
Jonataspaesdev <jonataspaesdev@gmail.com>
2026-02-02 11:44:10 -0300

    Primeiro commit com email correto

---
+++ src/br/com/jonataspaes/barbearia/Barbeiro.java
@@ -0,0 +1,5 @@
+package br.com.jonataspaes.barbearia;
+
+public class Barbeiro {
+
+}
---
+++ src/br/com/jonataspaes/barbearia/Main.java
@@ -0,0 +1,15 @@
+package br.com.jonataspaes.barbearia;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		System.out.println("+========================+");
+		System.out.println("+  Barbearia Dom Ribeiro +");
+		System.out.println("+========================+");
+
+		
+		
+	}
+
+}
