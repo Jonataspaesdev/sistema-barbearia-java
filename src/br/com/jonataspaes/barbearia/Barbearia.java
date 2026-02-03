3e670403f39430c2f7ffb077e7bf77ab8994a983
jonataspaesdev <jonataspaes.dev@gmail.com>
2026-02-02 22:14:51 -0300

    Incluindo classe Barbearia

--- src/br/com/jonataspaes/barbearia/Barbearia.java
+++ src/br/com/jonataspaes/barbearia/Barbearia.java
@@ -1,5 +1,40 @@
 package br.com.jonataspaes.barbearia;
 
+import java.util.ArrayList;
+
 public class Barbearia {
+	
+	private ArrayList<Barbeiro> barbeiros;
+	private ArrayList<Cliente> clientes;
+	
+	public Barbearia () {
+		
+		barbeiros = new ArrayList<>();
+		clientes = new ArrayList<>();
+		
+	}
+	
+	public void addBarbeiro(Barbeiro barber) {
+		
+		barbeiros.add(barber);
+		
+	}
+	
+	public void addCliente(Cliente client) {
+		
+		clientes.add(client);
+		
+	}
+	
+	public ArrayList<Barbeiro> getBarbeiros() {
+		
+		return barbeiros;
+		
+	}
+	
+	public ArrayList<Cliente> getClientes() {
+		
+		return clientes;
+	}
 
 }
